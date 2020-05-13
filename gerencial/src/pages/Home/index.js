d056ec4478208a8f5e24772c64d9c3a6408236e7
Antonio pc DevoluTI <raymendesjr2013@gmail.com>
2020-05-12 23:19:53 -0300

    Login Finalizado

--- gerencial/src/pages/Home/index.js
+++ gerencial/src/pages/Home/index.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Grid, Paper } from "@material-ui/core";
 
 const Home = () => {
-  return <div>Pagina de Home</div>;
+  useEffect(() => {
+    _handleLoad();
+  }, []);
+
+  const _handleLoad = () => {};
+
+  return (
+    <>
+      <Paper square>
+        <Grid container style={{}}>
+          <Grid item xs={12} md={12} lg={12}>
+            DashBoard
+          </Grid>
+        </Grid>
+      </Paper>
+    </>
+  );
 };
 
 export default Home;
--- gerencial/src/pages/Login/index.js
+++ gerencial/src/pages/Login/index.js
@@ -5,6 +5,7 @@ import {
   IconButton,
   Grid,
   CircularProgress,
+  Snackbar,
 } from "@material-ui/core";
 import Logo from "../../assets/images/logoColor.png";
 import Visibility from "@material-ui/icons/Visibility";
@@ -12,23 +13,49 @@ import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import { Redirect } from "react-router-dom";
 
 import { StyPaper, BoxLogo, BoxForms, BoxSubmit, BtnSubmit } from "./styled";
+import { post } from "../../services/Requests";
 
 const Login = () => {
+  const [state] = React.useState({
+    vertical: "top",
+    horizontal: "center",
+  });
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [seePwd, setSeePwd] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [mensErr, setMensErr] = useState("");
 
   const [isLoged, setLoged] = useState(false);
 
   const _handleSubmit = () => {
     setLoading(true);
     console.log("Apertou");
-    if (username && password) setLoged(true);
-    localStorage.setItem("token", "12345");
+    if (username && password) {
+      post("/usuario/login", { from: "web", username, password })
+        .then((res) => {
+          if (res) {
+            console.log("Resposta Login", JSON.stringify(res));
+            localStorage.setItem("token", res.token.token);
+            localStorage.setItem("user", JSON.stringify(res.usuario));
+            localStorage.setItem("username", res.usuario.username);
+            localStorage.setItem(
+              "categoria",
+              JSON.stringify(res.usuario.categoria)
+            );
+            setLoged(true);
+          }
+        })
+        .catch((err) => {
+          console.log("Erro no login", err.response);
+          setMensErr(
+            err.response ? err.response.data.message : "Contate o suporte"
+          );
+        });
+    }
     setLoading(false);
   };
-
+  const { vertical, horizontal } = state;
   return (
     <div className="container">
       {isLoged ? (
@@ -40,6 +67,20 @@ const Login = () => {
               <CircularProgress align="center" />
             </Grid>
           )}
+          <Snackbar
+            autoHideDuration={6000}
+            anchorOrigin={{ vertical, horizontal }}
+            key={`${vertical},${horizontal}`}
+            open={mensErr}
+            onClose={(event, reason) => {
+              if (reason === "clickaway") {
+                return;
+              }
+
+              setMensErr("");
+            }}
+            message={mensErr}
+          />
           <BoxLogo>
             <img
               alt="Logo"
