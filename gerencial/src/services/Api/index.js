6814c1ae0fa2861c85278f7b57d02d2bd7f2ab38
Antonio Raian <raymendesjr2013@gmail.com>
2020-05-14 17:13:08 -0300

    Mudanças pra o server

--- gerencial/src/services/Api/index.js
+++ gerencial/src/services/Api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = localStorage.getItem("token");
 
 const api = axios.create({
-  baseURL: "http://192.168.0.103:3333",
+  baseURL: "http://localhost:3333",
   headers: {
     Authorization: `Bearer ${token}`,
     "Content-Type": "application/json",
